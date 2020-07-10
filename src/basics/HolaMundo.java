5a67e3f03f4c2c82f88984818b76764ac6b65f28
rmorency40 <30129346+rmorency40@users.noreply.github.com>
2020-07-10 16:23:24 -0500

    Modify HoloMundo class in my project

--- src/basics/HolaMundo.java
+++ src/basics/HolaMundo.java
@@ -3,6 +3,7 @@ package basics;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo !");
+		System.out.println("I'm going to add codes to my project");
 	}
 
 }
